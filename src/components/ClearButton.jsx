a140b9b2c6057a84337813a0505cb3a52cc24156
cristiannoguera <cristian.noguera@clouxter.com>
2021-03-29 21:25:56 -0500

    29-marzo-problemas con eval

--- src/components/ClearButton.jsx
+++ src/components/ClearButton.jsx
@@ -1,6 +1,8 @@
-import React from 'react';
-import './ClearButton.css';
+import React from "react";
+import "./ClearButton.css";
 
-export const ClearButton = (props) => (
-    <div className="clear-btn">{this.props.children}</div>
+export const ClearButton = props => (
+    <div className="clear-btn" onClick={props.handleClear}>
+        {props.children}
+    </div>
 );
